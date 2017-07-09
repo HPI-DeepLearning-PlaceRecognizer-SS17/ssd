d0df436fe3c481dc94f53e09d0b78e5848463f9e
Arthur Silber <arthur@arthursilber.de>
2017-07-09 18:13:01 +0200

    Fixes flag

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ def parse_args():
     parser.add_argument('--voc07', dest='use_voc07_metric', type=bool, default=True,
                         help='use PASCAL VOC 07 11-point metric')
     parser.add_argument('--cpu', dest='cpu', type=bool, default=False, help='Use CPU for training. Not recommended.')
-    parser.add_argument('--visualBackprop', dest='useVisualBackprop', type=bool, action='store_true',
+    parser.add_argument('--visualBackprop', dest='useVisualBackprop', action='store_true',
                         help='Visualizes training with visual backprop')
 
     args = parser.parse_args()
